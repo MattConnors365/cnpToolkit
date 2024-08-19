c9545a6cc8b9725471bd634472495c7ff87619b7
Matt Connors <foxitb123@gmail.com>
2024-08-19 23:48:34 +0300

    Add function to generate 3-digit unique code

--- cnpUtils.js
+++ cnpUtils.js
@@ -1,3 +1,13 @@
+function getLength(number) {
+    return number.toString().length;
+}
+function generateUniqueCode() {
+    let uniqueCode = Math.floor((Math.random() * 999) + 1);
+    let codeLength = getLength(uniqueCode);
+    if (codeLength === 1) {uniqueCode = "00" + uniqueCode;}
+    else if (codeLength === 2) {uniqueCode = "0" + uniqueCode}
+    return uniqueCode;
+}
 
 function GenerateCNP(sex, yearOfBirth, monthOfBirth, dayOfBirth, countyCode) {
     
