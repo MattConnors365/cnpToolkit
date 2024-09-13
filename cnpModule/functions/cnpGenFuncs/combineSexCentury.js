ff2c4e063c7325ace5a64bb2425b628c84f9c4b2
Matt Connors <foxitb123@gmail.com>
2024-09-13 23:51:52 +0300

    Clarify combineSexAndCentury function
    Make clear which types the parameters should be and implemented some better error handling

--- cnpModule/functions/cnpGenFuncs/combineSexCentury.js
+++ cnpModule/functions/cnpGenFuncs/combineSexCentury.js
@@ -1,8 +1,14 @@
 
-function combineSexAndCentury(sexAtBirth, yearOfBirth, isForeigner) {
-    if (typeof isForeigner !== "boolean") {
-        throw new Error("Invalid type: isForeigner must be a boolean.");
-    }
+export default function combineSexAndCentury(sexAtBirth, yearOfBirth, isForeigner) {
+    // Parameter types expected, in order: string ('Male'/'Female'), number (4 digits), boolean
+    if (typeof sexAtBirth !== 'string') 
+        {throw new Error(`The sexAtBirth parameter must be assigned either the string value 'Male' or 'Female`)}
+    if (typeof yearOfBirth !== 'number' || yearOfBirth.toString().length !== 4)
+        {throw new Error(`The yearOfBirth parameter must be a number of 4 digits`)}
+    if (typeof isForeigner !== 'boolean') 
+        {throw new Error(`The isForeigner parameter must be a boolean`)}
+
+    // Validity checks
     if (yearOfBirth < 1800 || yearOfBirth > 2099) {
         alert(`${yearOfBirth} is not a year of birth currently supported by the CNP system. It only supports years between 1800 and 2099.`);
         throw new Error("Invalid year of birth: Supported timeframe between 1800-2099");
@@ -10,6 +16,8 @@ function combineSexAndCentury(sexAtBirth, yearOfBirth, isForeigner) {
     if (sexAtBirth !== "Male" && sexAtBirth !== "Female") {
         throw new Error("Invalid sex: CNPs only support the Male and Female sexes, assigned at birth.");
     }
+
+    // Logic
     const year = yearOfBirth.toString();
     const isMale = sexAtBirth === "Male";
     let digit = 0;
@@ -18,5 +26,6 @@ function combineSexAndCentury(sexAtBirth, yearOfBirth, isForeigner) {
     else if (year[1] === '8') {digit += 2} 
     else if (year[1] === '0') {digit += 4} 
     else if (year[1] === '9') {digit += 0}
+
     return digit;
 }
