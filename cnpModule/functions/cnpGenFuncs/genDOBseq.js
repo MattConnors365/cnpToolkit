40392989caae0a02992d84fb91d70ad795b104bf
Matt Connors <foxitb123@gmail.com>
2024-09-13 23:25:56 +0300

    Add checks to generateDateOfBirthSequence function
    Add checks to make sure the generateDateOfBirthSequence function's parameters are all strings

--- cnpModule/functions/cnpGenFuncs/genDOBseq.js
+++ cnpModule/functions/cnpGenFuncs/genDOBseq.js
@@ -1,23 +1,29 @@
 
 import { validMonths } from "../../constants";
 
-export default function generateDateOfBirthSequence(year, month, day) {
+export default function generateDateOfBirthSequence(year, month, day) { // Parameters expect strings
+    //Check if parameters are strings
+    if (typeof year !== 'string') {throw new Error(`The 'year' parameter needs to be a string!`)}
+    if (typeof month !== 'string') {throw new Error(`The 'month' parameter needs to be a string!`)}
+    if (typeof day !== 'string') {throw new Error(`The 'day' parameter needs to be a string!`)}
+    
     //Validate inputs
-    if (year < 1800 || year > 2099 || typeof year !== "number") {
+    if (Number(year) < 1800 || Number(year) > 2099 || typeof year !== "number") {
         throw new Error(`Invalid year: ${year} doesn't exist`);
     }
     const lowerCaseMonth = month.toLowerCase();
     if (!validMonths.includes(lowerCaseMonth)) {
         throw new Error(`Invalid month: ${month} doesn't exist`);
     }
-    if (day < 1 || day > 31 || typeof day !== "number") {
+    if (Number(day) < 1 || Number(day) > 31 || typeof day !== "number") {
         throw new Error(`Invalid day: ${day} doesn't exist`)
     }
 
     //Generate sequence
-    let yearDigits = year.toString().slice(-2);
+    let yearDigits = year.slice(-2);
     let monthDigits = (validMonths.indexOf(lowerCaseMonth) + 1).toString().padStart(2, '0');
-    let dayDigits = day.toString().padStart(2, '0');
+    let dayDigits = day.padStart(2, '0');
+
     let sequence = `${yearDigits}${monthDigits}${dayDigits}`;
 
     return sequence;
